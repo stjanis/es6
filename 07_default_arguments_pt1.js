3c8d72cb326cb740edb634f67c1c19f2ca4a28aa
stjanis <janis.stipnieks@novasol.com>
2016-05-06 11:29:16 +0200

    08: default arguments part 2

--- 07_default_arguments_pt1.js
+++ 07_default_arguments_pt1.js
@@ -1,3 +1,6 @@
 module.exports = function midpoint(minBound = 0, maxBound = 1) {
   return (minBound + maxBound) / 2;
 }
+
+// official solution
+// module.exports = (x = 0, y = 1) => (x + y) / 2;
---
+++ 08_default_arguments_pt2.js
@@ -0,0 +1,1 @@
+module.exports = (string, number = string.length) => string + '!'.repeat(number);
